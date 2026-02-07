cbd95b89b2702e96d934baae74e999ed7ed53778
Saad Tukur <saadtukur28@users.noreply.github.com>
2026-02-07 11:28:10 +0100

    Fix repo URL and update base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/rankbloom/',
+  base: '/rank_bloom-web/',
   // Basic configuration for a static site
   server: {
     port: 3000,
